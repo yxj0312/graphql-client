6576f6edabceafab35870a29decb059e9b5715fc
yxj0312 <yanxiajun@hotmail.com>
2022-02-07 23:05:11 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.list-wrapper {
+  display: flex;
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.list {
+  width: 50%;
+}
+</style>
